2a258c9ab7d06c4f04a17454645c596e81a13716
Eshu Thakurathi <eshu2704@gmail.com>
2025-02-14 21:19:27 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,20 @@
+// let a = "Mummy"
+// let b = 10;
+// console.log(a+b)
+// console.log("Hello")
+// const p ={
+//     name: "Lokesh",
+//     section:2,
+//     marks:80
+// }
+// console.log(p)
+let age = 23
+
+age = Number.parseInt(age)
+
+if(age>20 && age<30){
+    console.log("Your age lies between 20 and 30")
+}
+else{
+    console.log("Your age does not lies between 20 and 30")
+}

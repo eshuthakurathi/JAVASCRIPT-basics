b67a64f7ac1ef45bc3376c1d2233aec145f60a87
Eshu Thakurathi <eshu2704@gmail.com>
2025-02-16 11:25:21 +0530

    Changing html classes

---
+++ Classes/scrip.js
@@ -0,0 +1,6 @@
+let first = document.getElementById("first");
+first.className = "text-black red"
+// first.classList.add("red")
+first.classList.remove("red")
+// first.classList.contains("red")
+// first.classList.toggle("red")

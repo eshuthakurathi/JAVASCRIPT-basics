73fb48eb7345e62d27e0e87dc722876f6b400af6
Eshu Thakurathi <eshu2704@gmail.com>
2025-02-16 11:05:40 +0530

    Inserting

--- Insertion methods/script.js
+++ Insertion methods/script.js
@@ -3,8 +3,16 @@ let a =document.getElementsByTagName('div')[0]
 
 let div = document.createElement('div');
 div.innerHTML = '<h1>Hello World!</h1>';
+
+
 // a.append(div);
 // a.prepend(div);
 // a.before(div);
 // a.after(div);
-a.replaceWith(div);
+// a.replaceWith(div);
+
+// first.insertAdjacentHTML('beforebegin', '<div class="test">beforebegin</div>');
+// first.insertAdjacentHTML('beforeend', '<div class="test">beforeend</div>');
+// first.insertAdjacentHTML('afterbegin', '<div class="test">afterbegin</div>');
+// first.insertAdjacentHTML('afterend', '<div class="test">afterend</div>');
+first.remove()

4bc40f75aec59c839892c7b78971a4d7a947e296
Eshu Thakurathi <eshu2704@gmail.com>
2025-02-17 19:03:22 +0530

    html attributes

--- AttributeMethods/script.js
+++ AttributeMethods/script.js
@@ -1,14 +1,25 @@
+// our element which is accessed by the id
 let first = document.getElementById("first");
 
+// to get the value of an attribute
+let a = first.getAttribute("class")
+console.log(a)
 
-// let a = first.getAttribute("class")
-// console.log(a)
-// console.log(first.hasAttribute("class"))
-// console.log(first.hasAttribute("style"))
-// first.setAttribute("hidden", "true")
-// first.setAttribute("class", "true sachin")
-// first.removeAttribute("class")
-// console.log(first.attributes)
+// check for existence of an attribute
+console.log(first.hasAttribute("class"))
+console.log(first.hasAttribute("style"))
+
+// to set value of an attribute
+first.setAttribute("hidden", "true")
+first.setAttribute("class", "true sachin")
+
+// to remove an attribute from the element
+first.removeAttribute("class")
+
+// to get the collection of all attributes
+console.log(first.attributes)
+
+// to access our custom attributes
 console.log(first.dataset)
 console.log(first.dataset.game)
 console.log(first.dataset.player)

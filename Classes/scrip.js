42f3e394ab38cc8e085c10378975e51fcf7da05b
Eshu Thakurathi <eshu2704@gmail.com>
2025-02-17 19:33:29 +0530

    working with html classes

--- Classes/scrip.js
+++ Classes/scrip.js
@@ -1,6 +1,16 @@
 let first = document.getElementById("first");
+
+// when something is assigned to elem.className, it replaces the whole string of classes
 first.className = "text-black red"
-// first.classList.add("red")
+
+// classList is the list of classes in the class
+
+// adds/removes a class
+first.classList.add("red")
 first.classList.remove("red")
-// first.classList.contains("red")
-// first.classList.toggle("red")
+
+// checks for the given class, returns true/false
+first.classList.contains("red")
+
+// adds the class if it exist otherwise removes it
+first.classList.toggle("red")

c168da0271c7e65d3637482649f730dab5ec3d6e
Eshu Thakurathi <eshu2704@gmail.com>
2025-02-17 19:55:30 +0530

    setInterval and setTimeout

--- SetMethods/script.js
+++ SetMethods/script.js
@@ -5,17 +5,25 @@ const sum = (a, b, c) => {
   a + b
 }
 
+// calling fucntion using settimeout
 setTimeout(sum, 1000, 1, 2, 7)
-// setInterval(function() {
+
+// runs again an again after the given interval of time
+// let c= setInterval(function() {
 //   alert("setinterval")
 // }, 3000)
 
-// let a = setTimeout(function() {
-//   alert("I am inside of settimeout")
-// }, 5000)
 
-// let b = prompt("Do you want to run the settimout?")
-// if ("n" == b) {
-//   clearTimeout(a)
-// }
-// console.log(a)
+// allows us to run a function once after the given interval of time
+let a = setTimeout(function() {
+  alert("I am inside of settimeout")
+}, 5000)
+
+
+// a condition for clearing timeout or not
+let b = prompt("Do you want to run the settimout?")
+if ("n" == b) {
+  clearTimeout(a) //to cancel the execution of our settimeout
+}
+
+console.log(a) //gives the timer id in console window

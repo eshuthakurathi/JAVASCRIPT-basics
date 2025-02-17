6a8aa7747bb43260deac376364ee93c9190de9e7
Eshu Thakurathi <eshu2704@gmail.com>
2025-02-17 20:21:03 +0530

    Handling browsing events

--- Events/script.js
+++ Events/script.js
@@ -1,28 +1,31 @@
-// let a = document.getElementsByClassName("container")[0]
-// a.onclick = () => {
-//   let b = document.getElementsByClassName("container")[0]
-//   b.innerHTML = "Hello World!"
-// }
+let a = document.getElementsByClassName("container")[0]
+
+// events being handled using onclick property
+// adding a handler with js overwrites the existing handler
+a.onclick = () => {
+  let b = document.getElementsByClassName("container")[0]
+  b.innerHTML = "Hello World!"
+}
 
 let x = function(event) {
-    console.log(event.target)
-    console.log(event.type, event.clientX, event.clientY)
-    // alert("Hello World1!")
-  }
+  console.log(event.target) //gives the target that is in which button we click for the event to occur
+  console.log(event.type, event.clientX, event.clientY)  //event type ; coordinates of the cursor 
+  alert("Hello World1!")
+}
   
-  let y = function(e) {
-    console.log(e)
-    alert("Hello World2!")
-  }
+let y = function(e) {
+  console.log(e)  //e is an event object, shows all the properties of it
+  alert("Hello World2!")
+}
+
+
+// addEventListener is used to assign multiple handlers to an event
+btn.addEventListener('click', x)
   
-  btn.addEventListener('click', x)
+btn.addEventListener('click', y)
   
-  
-  // btn.addEventListener('click', y)
-  
-  // let a = prompt("What is your favorite number?");
-  
-  
-  // if (a == "2") {
-  //   btn.removeEventListener('click', x)
-  // }
+
+let c = prompt("What is your favorite number?");  
+if (c == "2") {
+  btn.removeEventListener('click', x) //only works when our func. object are same
+}
